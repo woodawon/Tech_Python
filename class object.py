b0b5bc36c597e1a51f70417eaa3811cbaf59369a
우다원 <105121244+woodawon@users.noreply.github.com>
2022-07-22 14:50:12 +0000

    클래스

--- class object.py
+++ class object.py
@@ -1,6 +1,15 @@
 class Person:
-  def say_hello(self):
-    print("안녕 나는 " + self.name)
+  # constructor - 생성자
+  def __init__(self, a, b):
+    self.name = a
+    self.age = b
 
-p = Person()
-p.say_hello()
+  def say_hello(self):
+    print("안녕 나는 " 
+          + self.name + 
+         " 그리고 나는"
+         + str(self.age) + 
+          " 살이야")
+
+wonie = Person("워니", 20)
+wonie.say_hello()
