cfbd9e6e56bc5d4b05b18389f05761a32107dd01
우다원 <105121244+woodawon@users.noreply.github.com>
2022-07-23 10:36:19 +0000

    함수

--- main.py
+++ main.py
@@ -4,19 +4,16 @@
 def greet(name):
     print(f'안녕 {name}, 잘 지내니?')
 
-
 greet("민지")
 
 
 def hello_world():
     print("Hello World")
 
-
 hello_world()
 
 
 def greet(name, age, location):
     print(f"안녕 {name}, 넌 {age}살이고 {location}에 사는구나~")
-
 
 greet("다원", 17, "서울")
